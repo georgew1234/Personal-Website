0ed59198912aed0ed02d32fc1ec1a25e81615a9d
George Wen <gw2259@nyu.edu>
2025-09-29 09:26:44 +0800

    Updated Portfolio

--- src/app/components/portfolio.tsx
+++ src/app/components/portfolio.tsx
@@ -3,7 +3,16 @@ export default function Portfolio() {
     return (
         <>
             <h2 className="text-2xl text-header">Portfolio</h2>
-            <ProjectCard href="google.com" title="fuck" description="it"/>
+            <ProjectCard title="NYU University Award Web Application" description="Developed and maintained web applications for NYU’s HR Department, including university awards management APP. Troubleshooted many bugs with the web application and the development environment.
+Provided user support, praised for great communication skills, ability  to explain tech issues to non tech people.
+Development work involves both backend (Java, Oracle Database) and frontend (HTML, CSS, JavaScript, Thymeleaf). Technology used including Spring Framework and Weblogic
+
+"/>
+            <ProjectCard title="Pokemon Showdown Win Percentage Predictor" description="The project runs on a cloud server and stores data logs in cloud storage in AWS.
+Python web scraping pipeline is used to extract replay data from Pokémon Showdown.
+A log parsing system was built to convert the data into structured battle states for each turn.
+The output data was used to build a neural network in PyTorch to predict a player's win probability at any given turn based on the state of the battle.
+"/>
         </>
     );
 }
--- src/app/components/project-card.tsx
+++ src/app/components/project-card.tsx
@@ -1,7 +1,6 @@
-export default function ProjectCard ({href, title, description}: {href: string, title: string, description: string}) {
+export default function ProjectCard ({title, description}: {title: string, description: string}) {
     return (
         <a
-            href={href}
             className="block p-6 rounded-2xl shadow-md transition-transform transform hover:shadow-xl no-underline"
         >
             <h2 className="text-xl text-header">{title}</h2>
